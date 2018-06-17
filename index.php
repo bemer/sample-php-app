bc510c5c9b4bf39ea9bf3103a095cdbe58b624d9
Bruno Emer <brunoemer@gmail.com>
2018-06-17 19:49:37 -0300

    Changing text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <body>
 <div align="center">
-                <h1>Hello Idenize:</h1>
+                <h1>Hello there!</h1>
                 <h2>This is a sample PHP application deployed with OpsWorks!</h2>
                 <div align="center"><img src="https://s3.amazonaws.com/brunemer/ops-works-logo.png"></div>
                 <p>You PHP Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
