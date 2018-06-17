965a643fd7749a5045d73a8d9e4adef6198a5155
Bruno Emer <brunoemer@gmail.com>
2018-06-17 19:36:17 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Cloud Gurus</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello There</h1>
+                <h2>This is a sample PHP application deployed with OpsWorks!</h2>
+                <div align="center"><img src="https://s3.amazonaws.com/brunemer/ops-works-logo.png"></div>
+                <p>You PHP Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
+
